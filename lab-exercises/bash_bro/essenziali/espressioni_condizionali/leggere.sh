8e238030e074cecebe9a813cf300383cbe661343
Mounir <mounir.samite@gmail.com>
2023-01-13 20:43:19 +0100

    before the end

--- lab-exercises/bash_bro/essenziali/espressioni_condizionali/leggere.sh
+++ lab-exercises/bash_bro/essenziali/espressioni_condizionali/leggere.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#/bin/bash
 exec {FD}</usr/include/stdio.h
 if (( $? == 0 )) ; then
-	while read -u ${FD} A B C D;do
+	while read -u ${FD} A B C ;do
 		echo "$C"
 	done
 	exec {FD}>&-
 fi
+
+
+while read A B C D; do
+	echo $C >> pippo.txt
+done < /usr/include/stdio.h
